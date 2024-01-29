0185a057a457997ad619ecdeb7aed3c009b54318
hitsuji24 <2xx4xx.mole.melajis@gmail.com>
2024-01-30 01:22:19 +0900

    書き始め

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>データ登録</title>
+  <link  rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<!-- <form method="POST" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>フリーアンケート</legend>
+     <label>名前：<input type="text" name="name"></label><br>
+     <label>Email：<input type="text" name="email"></label><br>
+     <label>年齢：<input type="text" name="age"></label><br>
+     <label><textArea name="naiyou" rows="4" cols="40"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form> -->
+
+<!--  ビジネスモデルキャンバスの書き込みフォーマットを用意する-->
+<table>
+	<tbody>
+		<tr>
+			<td rowspan="2"></td>
+			<td></td>
+			<td rowspan="2"></td>
+			<td></td>
+			<td rowspan="2"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td colspan="3"></td>
+			<td colspan="2"></td>
+		</tr>
+	</tbody>
+</table>
+<!-- Main[End] -->
+
+
+</body>
+</html>
